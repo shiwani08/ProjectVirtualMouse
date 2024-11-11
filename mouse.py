b3a351c575840d523e1a4957b71ef4563ff6e222
ShiwaniCC <shiwanisoni29082002@gmail.com>
2024-11-11 22:49:41 +0530

    Left click working, right click is not

--- mouse.py
+++ mouse.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import pyautogui
+
 cap = cv2.VideoCapture(0)
 hand_detector = mp.solutions.hands.Hands()
 drawing = mp.solutions.drawing_utils
@@ -15,6 +16,7 @@ while True:
     output = hand_detector.process(rgb_frame)
     hands = output.multi_hand_landmarks
     print(hands)
+
     if hands:
         for hand in hands:
             drawing.draw_landmarks(frame, hand)
@@ -23,21 +25,33 @@ while True:
                 x = int(landmark.x * frame_width)
                 y = int(landmark.y * frame_height)
 
-                if id == 8:
-                    cv2.circle(img = frame, center = (x,y), radius = 15, color = (0, 0, 255 ))
+                if id == 8:  # Tip of the index finger
+                    cv2.circle(img = frame, center = (x, y), radius = 15, color = (0, 0, 255))
                     index_x = screen_width / frame_width * x
                     index_y = screen_height / frame_height * y
                     pyautogui.moveTo(index_x, index_y)
-                
-                if id == 4:
-                    cv2.circle(img = frame, center = (x,y), radius = 15, color = (255, 0, 0))
+
+                if id == 4:  # Tip of the thumb
+                    cv2.circle(img = frame, center = (x, y), radius = 15, color = (255, 0, 0))
                     thumb_x = screen_width / frame_width * x
                     thumb_y = screen_height / frame_height * y
                     print('outside', abs(index_y - thumb_y))
+
+                    # Left Click when index and thumb are close
                     if abs(index_y - thumb_y) < 60:
                         pyautogui.click()
                         pyautogui.sleep(1)
 
+                # Right Click when thumb and middle finger are close
+                if id == 12:  # Tip of the middle finger
+                    middle_x = int(landmarks[12].x * frame_width)
+                    middle_y = int(landmarks[12].y * frame_height)
+                    print('middle finger', abs(thumb_y - middle_y))
+
+                    # Right Click when thumb and middle finger are close
+                    if abs(thumb_y - middle_y) < 60:
+                        pyautogui.rightClick()
+                        pyautogui.sleep(1)
 
     cv2.imshow("This is your HANDY mouse", frame)
     cv2.waitKey(1)
